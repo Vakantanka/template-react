0b94387dd1ac27a108c2e4a1ad4c75dc192e6e9d
László Fenyvesi <laszlo.fenyvesi@gmail.com>
2022-05-24 09:46:00 +0200

    middleware start

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const logger = require('./middleware/logger.js')
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
